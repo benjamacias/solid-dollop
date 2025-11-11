a4b9a38d0788f2c6cdfbaabc6e1b773236bbe838
benjamacias <74431154+benjamacias@users.noreply.github.com>
2025-11-11 21:30:27 +0100

    Redirect home page to newsbm-tech.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import LandingPage from './components/LandingPage';
+"use client";
+
+import { useEffect } from "react";
+
+const REDIRECT_URL = "https://newsbm-tech.com/";
 
 export default function Home() {
-  return <LandingPage />;
+  useEffect(() => {
+    window.location.replace(REDIRECT_URL);
+  }, []);
+
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
+      <p className="text-lg">
+        Redirigiendo a <a className="underline" href={REDIRECT_URL}>newsbm-tech.com</a>...
+      </p>
+    </main>
+  );
 }
